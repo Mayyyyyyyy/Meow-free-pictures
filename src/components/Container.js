37c45728198da09c49912d60c1a983f6e7b92b61
Mayyyyyyyy <8633009+mayyyyyyyy@user.noreply.gitee.com>
2021-02-28 18:19:25 +0800

    fix some little bugs

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,23 +3,23 @@ const Container = ({images}) => {
     return ( 
         <div className="container">
          {images.map(image=>(
-             <div className="card" id={image.id}>
+             <div className="card" key={image.id}>
                <div className="image">
-                 <img src={image.previewURL} alt="picture"></img>
+                 <img src={image.previewURL} alt="different things"></img>
                </div>
                <div className="imageInfo">
                 <div>
-                        <p><i class="fa fa-user-circle" aria-hidden="true"></i> : {image.user}</p>
+                        <p><i className="fa fa-user-circle" aria-hidden="true"></i> : {image.user}</p>
                 </div>
                 <div>
-                        <p><i class="fa fa-eye" aria-hidden="true"></i>: {image.views}</p>
-                        <p><i class="fa fa-thumbs-up" aria-hidden="true"></i>: {image.likes}</p>
-                        <p><i class="fa fa-download" aria-hidden="true"></i>: {image.downloads}</p>  
-                        <p><i class="fa fa-tags" aria-hidden="true"></i> : {image.tags.split(",").map(tag=>`#${tag}`)}</p>       
+                        <p><i className="fa fa-eye" aria-hidden="true"></i>: {image.views}</p>
+                        <p><i className="fa fa-thumbs-up" aria-hidden="true"></i>: {image.likes}</p>
+                        <p><i className="fa fa-download" aria-hidden="true"></i>: {image.downloads}</p>  
+                        <p><i className="fa fa-tags" aria-hidden="true"></i> : {image.tags.split(",").map(tag=>`#${tag}`)}</p>       
                 </div>
                </div>
                <div>
-                 <a href={image.pageURL} target="_blank"><button>Download</button></a>
+                 <a href={image.pageURL} target="_blank" rel="noreferrer"><button>Download</button></a>
                </div>
              </div>
          ))}
